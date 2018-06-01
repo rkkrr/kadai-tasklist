561a196cc99c518dddf8882df57b214a292ecc53
jiro.kirameki <ec2-user@ip-172-31-18-27.us-east-2.compute.internal>
2018-06-01 05:23:14 +0000

    CRUD messages

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $tasks->id }} のメッセージ詳細ページ</h1>
+
+    <p>{{ $tasks->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $tasks->id]) !!}
+    {!! Form::model($tasks, ['route' => ['tasks.destroy', $tasks->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
