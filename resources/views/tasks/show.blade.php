0d005f867ccd63befb4c2846adc251f3d78f4d1a
jiro.kirameki <ec2-user@ip-172-31-18-27.us-east-2.compute.internal>
2018-06-06 06:41:24 +0000

    kadai bootstrap2

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,7 +5,7 @@
 <h1>id = {{ $tasks->id }} のメッセージ詳細ページ</h1>
 
  <div class="row">
-        <div class="col-sm-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-md-6" >
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6" >
         
      <table class="table table-bordered">
         <tr>
